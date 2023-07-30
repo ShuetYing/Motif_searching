59dbfad5bb7213240dfbae3f0d248b179ce7b98f
ShuetYing <114529265+ShuetYing@users.noreply.github.com>
2023-07-30 15:10:30 +0100

    Create cleanup.py

---
+++ cleanup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import os
+
+# remove all the process file, only remaining files with result output
+
+os.remove("two_lines_seq.fa")
+os.remove("one_line_seq.fa")
+os.remove("remove_partial_seq.fa")
+os.remove("header.txt")
+os.remove("merged_option.fa")
+os.remove("seq_wanted.fa")
+os.remove("seq_wanted2.fa")
+os.remove("interested_sequences.fa")
+os.remove("motif_input.fa")
+os.remove("motif.txt")
+os.remove("motif1.txt")
+os.remove("ID_species.txt")
+os.remove("antigenic.txt")
+os.remove("MSA.fa")
+os.remove("binding_motif.txt")
+os.remove("binding_motif1.txt")
+os.remove("binding_motif2.txt")
+os.remove("binding_motif_gff.txt")
+os.remove("binding_motif_excel.txt")
+os.remove("binding_motif_seqtable.txt")
+os.remove("bm_ID.txt")
+os.remove("species.txt")
+os.remove("ID.txt")
+os.remove("plotcon1.dat")
